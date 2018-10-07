6c97fe263157b709a55f79d3d91542d45a40c337
AlexandreCote <acoteomg@gmail.com>
2018-10-06 21:14:25 -0400

    arggg

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+
+    private void Awake()
+    {
+        Object.DontDestroyOnLoad(this);
+    }
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadNextScene", 5f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(nextSceneIndex);
+    }
+}
