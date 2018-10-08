d41a9584cdf677bcfedbbc386ad1a61058aed7b8
AlexandreCote <acoteomg@gmail.com>
2018-10-08 16:11:41 -0400

    Tidying

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadNextScene", 5f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(nextSceneIndex);
+    }
+}
